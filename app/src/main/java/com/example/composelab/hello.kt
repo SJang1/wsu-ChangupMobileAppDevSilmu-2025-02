3c73a1317e526aa5419702f39ecf15fead15e62a
SJang1 <siliconsjang@gmail.com>
2025-09-09 17:09:19 +0900

    hello

--- app/src/main/java/com/example/composelab/hello.kt
+++ app/src/main/java/com/example/composelab/hello.kt
@@ -1,5 +1,9 @@
 package com.example.composelab
 
+
+val data1: Any = 10 // Any type
+val data2: Nothing? = null // only null
+
 fun main() {
     println("Hello World")
 
@@ -11,6 +15,41 @@ fun main() {
         return sum
     }
 
+    var data = 10
+    var result = if (data < 5) { // result = false
+        print(" data < 5")
+        true
+    } else {
+        print("data >= 5")
+        false
+    }
+
     val name: String = "Sae Jin Kim"
     println("name: $name, sum: ${sum(10)}, plus: ${10 + 20}")
+}
+
+
+fun 조건문() {
+
+    var n = 8
+
+    if (n < 1 || n > 9) {
+        println("1부터 9까지의 숫자를 입력해주세요.")
+    } else {
+        when (n) {
+            1 -> menuGuestInfo(userName, userMobile)
+            2 -> menuCartItemList()
+            3 -> menuCartClear()
+            4 -> menuCartAddItem(mBook)
+            5 -> menuCartRemoveItemCount()
+            6 -> menuCartRemoveItem()
+            7 -> menuCartBill()
+            8 -> {
+                menuExit()
+                quit = true
+            }
+
+            9 -> menuAdminLogin()
+        }
+    }
 }
