dcaf0f644a5333ad3cef25cef09b08de5f7282d8
SJang1 <siliconsjang@gmail.com>
2025-09-16 15:43:56 +0900

    add image to home screen, add README

--- 0916/app/src/main/java/com/example/a0916/MainActivity.kt
+++ 0916/app/src/main/java/com/example/a0916/MainActivity.kt
@@ -1,47 +1,58 @@
 package com.example.a0916
 
+import android.media.Image
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import com.example.a0916.ui.theme._0916Theme
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.size
+import androidx.compose.ui.unit.dp
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContent {
-            _0916Theme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
-                }
-            }
+            HomeScreen()
         }
     }
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun HomeScreen() {
+    Column(
+        modifier = Modifier.fillMaxSize(),  // fill the page to the max size of device
+        horizontalAlignment = Alignment.CenterHorizontally, // center horizontally
+        verticalArrangement = Arrangement.Center    // center vertically
+    ) {
+        Text("Compose Coffee")
+        Image(
+            painter = painterResource(id = R.drawable.compose),
+            contentDescription = "Jetpack Compose Logo",
+            modifier = Modifier
+                .size(300.dp)
+                .padding(16.dp)
+        )
+        Text("위치: 우송대 정문 앞")
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
-    _0916Theme {
-        Greeting("Android")
-    }
+fun HomeScreenPreview() {
+    HomeScreen()
 }
+
---
+++ README.md
@@ -0,0 +1,4 @@
+# wsu-ChangupMobileAppDevSilmu-2025-02
+### 우송대학교 2025학년도 2학기 창업앱개발실무 과제
+각 폴더는 각각의 Project입니다.  
+Open Project 시 내부 폴더 (날짜정리) 에서 Open 해 주세요.
