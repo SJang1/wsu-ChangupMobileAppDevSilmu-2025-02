b74527a977816038393ff4de8abd56eb20cba8f3
SJang1 <siliconsjang@gmail.com>
2025-11-11 15:41:03 +0900

    11/11 cupcake

---
+++ cupcake/app/src/main/java/com/composelab/cupcake/MainActivity.kt
@@ -0,0 +1,24 @@
+package com.composelab.cupcake
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.lifecycle.viewmodel.compose.viewModel
+import com.composelab.cupcake.ui.theme.CupcakeTheme
+import com.composelab.cupcake.viewmodel.OrderViewModel
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContent {
+            CupcakeTheme {
+                Surface(color = MaterialTheme.colorScheme.background) {
+                    val orderViewModel: OrderViewModel = viewModel()
+                    CupcakeApp(viewModel = orderViewModel)
+                }
+            }
+        }
+    }
+}
---
+++ cupcake/app/src/main/java/com/composelab/cupcake/navigation/CupcakeScreen.kt
@@ -0,0 +1,8 @@
+package com.composelab.cupcake.navigation
+
+sealed interface CupcakeScreen {
+    data object Start : CupcakeScreen
+    data object Flavor : CupcakeScreen
+    data object Pickup : CupcakeScreen
+    data object Summary : CupcakeScreen
+}
